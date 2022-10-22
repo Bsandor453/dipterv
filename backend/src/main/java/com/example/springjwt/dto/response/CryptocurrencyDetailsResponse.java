971ede0264c37adfa15df43a91796505e411c369
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-10-23 01:02:18 +0200

    Add JsonProperty to response classes

--- backend/src/main/java/com/example/springjwt/dto/response/CryptocurrencyDetailsResponse.java
+++ backend/src/main/java/com/example/springjwt/dto/response/CryptocurrencyDetailsResponse.java
@@ -2,6 +2,7 @@ package com.example.springjwt.dto.response;
 
 import com.example.springjwt.models.cryptocurrency.details.CryptocurrencyCommunityData;
 import com.example.springjwt.models.cryptocurrency.details.CryptocurrencyDeveloperData;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,30 +10,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CryptocurrencyDetailsResponse {
 
+    @JsonProperty("id")
     private String id;
 
+    @JsonProperty("description")
     private String description;
 
+    @JsonProperty("links")
     private CryptocurrencyLinksResponse links;
 
+    @JsonProperty("genesis_date")
     private String genesisDate;
 
+    @JsonProperty("sentiment_votes_up_percentage")
     private Double votesUpPercentage;
 
+    @JsonProperty("sentiment_votes_down_percentage")
     private Double votesDownPercentage;
 
+    @JsonProperty("coingecko_score")
     private Double coingeckoScore;
 
+    @JsonProperty("developer_score")
     private Double developerScore;
 
+    @JsonProperty("community_score")
     private Double communityScore;
 
+    @JsonProperty("liquidity_score")
     private Double liquidityScore;
 
+    @JsonProperty("public_interest_score")
     private Double publicInterestScore;
 
+    @JsonProperty("community_data")
     private CryptocurrencyCommunityData communityData;
 
+    @JsonProperty("developer_data")
     private CryptocurrencyDeveloperData developerData;
 
 }
--- backend/src/main/java/com/example/springjwt/dto/response/CryptocurrencyLinksResponse.java
+++ backend/src/main/java/com/example/springjwt/dto/response/CryptocurrencyLinksResponse.java
@@ -1,5 +1,6 @@
 package com.example.springjwt.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,20 +10,28 @@ import java.util.List;
 @NoArgsConstructor
 public class CryptocurrencyLinksResponse {
 
+    @JsonProperty("homepage")
     private List<String> homepage;
 
+    @JsonProperty("blockchain_site")
     private List<String> blockchainSite;
 
+    @JsonProperty("official_forum_url")
     private List<String> officialForumUrl;
 
+    @JsonProperty("chat_url")
     private List<String> chatUrl;
 
+    @JsonProperty("announcement_url")
     private List<String> announcementUrl;
 
+    @JsonProperty("twitter_screen_name")
     private String twitterUsername;
 
+    @JsonProperty("subreddit_url")
     private String subredditUrl;
 
+    @JsonProperty("github_repos")
     private List<String> githubRepos;
 
 }
