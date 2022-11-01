af17bd24e22239fe621ee0aa74b9d4f1ce12e91d
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-11-01 23:37:25 +0100

    Add prettier

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const App = () => {
   return (
@@ -13,9 +13,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
