493a6302398c0c04b1c9123ffd5e55bf25389e41
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-11-20 01:11:44 +0100

    Remove serializable check from store

--- mobile/src/redux/store.ts
+++ mobile/src/redux/store.ts
@@ -11,6 +11,10 @@ export const store = configureStore({
     snackbar: snackbarReducer,
     crypto: cryptocurrencyReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
