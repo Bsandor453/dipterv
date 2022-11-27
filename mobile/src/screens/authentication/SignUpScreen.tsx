1e3afd8d00d87cd6fdef7ca6da43345f89686552
Bereczki Sándor Máté <bsandor453@gmail.com>
2022-11-28 00:59:27 +0100

    Minor padding fix on sign in screen

--- mobile/src/screens/authentication/SignUpScreen.tsx
+++ mobile/src/screens/authentication/SignUpScreen.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
     paddingHorizontal: 0,
-    paddingVertical: 30,
+    paddingTop: 30,
   },
   textCard: {
     color: TextColor,
